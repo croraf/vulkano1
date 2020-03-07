ded56d4b5d50d47ed303c35cc930151f44db63fd
croraf <vrafaeli@msn.com>
2020-03-07 21:33:37 +0100

    Increase the number of points and make coordinates double

--- src/main.rs
+++ src/main.rs
@@ -50,20 +50,19 @@ fn main() {
 
     let mut rng = thread_rng();
 
-    let data_in_iter = (0..2048).map(|_| (rng.gen_range(0, 100), rng.gen_range(0, 100)));
-    let mut rng = thread_rng();
-    let data_in_iter2 = (0..2048).map(|_| (rng.gen_range(0, 100), rng.gen_range(0, 100)));
+    let lower_bound = 0.0;
+    let upper_bound = 100.0;
+    let data_in_iter = (0..1024*100).map(|_| (rng.gen_range(lower_bound, upper_bound), rng.gen_range(lower_bound, upper_bound)));
+    /* let data_in_iter2 = (0..1024*100).map(|_| (rng.gen_range(lower_bound, upper_bound), rng.gen_range(lower_bound, upper_bound)));
     for (x, y) in data_in_iter2 {
         println!("x: {} y: {}", x, y);
-    }
+    } */
 
     let data_in_buffer =
         CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, data_in_iter)
             .expect("failed to create buffer");
-
-
-
-    let data_out_iter = (0..2048).map(|_| -1);
+            
+    let data_out_iter = (0..1024*100).map(|_| -1);
     let data_out_buffer =
         CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, data_out_iter)
             .expect("failed to create buffer");
@@ -79,7 +78,7 @@ fn main() {
                     layout(local_size_x = 1024, local_size_y = 1, local_size_z = 1) in;
                     
                     layout(set = 0, binding = 0) buffer DataIn {
-                        uvec2 data[];
+                        dvec2 data[];
                     } buf_in;
     
                     layout(set = 0, binding = 1) buffer DataOut {
@@ -89,7 +88,7 @@ fn main() {
                     void main() {
                         uint idx = gl_GlobalInvocationID.x;
                         double dist_squared = (buf_in.data[idx].x - 50) * (buf_in.data[idx].x - 50) + (buf_in.data[idx].y - 50) * (buf_in.data[idx].y - 50);
-                        buf_out.data[idx] =  dist_squared < 25 ? 1 : 0;
+                        buf_out.data[idx] =  dist_squared <= 25 ? 1 : 0;
                     }
                 "
             }
@@ -113,7 +112,7 @@ fn main() {
     let command_buffer =
         AutoCommandBufferBuilder::primary_one_time_submit(device.clone(), queue.family())
             .unwrap()
-            .dispatch([2, 1, 1], compute_pipeline.clone(), set.clone(), ())
+            .dispatch([100, 1, 1], compute_pipeline.clone(), set.clone(), ())
             .unwrap()
             .build()
             .unwrap();
@@ -130,7 +129,7 @@ fn main() {
     let mut n_colliding = 0;
     for (n, val) in content.iter().enumerate() {
         n_colliding += val;
-        println!("{} {:?}", n, val);
+        /* println!("{} {:?}", n, val); */
     }
 
     println!("{}", n_colliding);
